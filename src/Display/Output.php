bde63594dfcd91ad14c8b72b744bcde109a30a95
Baptiste Langlade <baptiste.langlade@hey.com>
2025-08-24 12:02:08 +0200

    update dependencies

--- src/Display/Output.php
+++ src/Display/Output.php
@@ -4,12 +4,18 @@ declare(strict_types = 1);
 namespace Innmind\Async\HttpServer\Display;
 
 use Innmind\CLI\Console;
-use Innmind\Immutable\Str;
+use Innmind\Immutable\{
+    Attempt,
+    Str,
+};
 
 /**
  * @psalm-immutable
  */
 interface Output
 {
-    public function __invoke(Console $env, Str $data): Console;
+    /**
+     * @return Attempt<Console>
+     */
+    public function __invoke(Console $env, Str $data): Attempt;
 }
